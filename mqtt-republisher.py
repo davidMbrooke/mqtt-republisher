99b88b44cd54fbac24c24e98a51f2f4801c06f40
davidMbrooke <davidmbrooke@users.sourceforge.net>
2018-08-27 12:52:19 +0000

    Changed to use paho client and added authentication

--- mqtt-republisher.py
+++ mqtt-republisher.py
@@ -13,7 +13,7 @@ import time
 import socket
 import sys
 
-import mosquitto
+import paho.mqtt.client as mqtt
 import ConfigParser
 import setproctitle
 
@@ -28,12 +28,14 @@ MAPFILE = config.get("global", "mapfile")
 MQTT_HOST = config.get("global", "mqtt_host")
 MQTT_PORT = config.getint("global", "mqtt_port")
 MQTT_TOPIC = config.get("global", "mqtt_topic")
+MQTT_USER = config.get("global", "mqtt_user")
+MQTT_PWD = config.get("global", "mqtt_pwd")
 
 APPNAME = "mqtt-republisher"
 PRESENCETOPIC = "clients/" + socket.getfqdn() + "/" + APPNAME + "/state"
 setproctitle.setproctitle(APPNAME)
 client_id = APPNAME + "_%d" % os.getpid()
-mqttc = mosquitto.Mosquitto(client_id)
+mqttc = mqtt.Client(client_id)
 
 LOGFORMAT = '%(asctime)-15s %(message)s'
 
@@ -53,28 +55,28 @@ logging.debug("DEBUG MODE")
 # All the MQTT callbacks start here
 
 
-def on_publish(mosq, obj, mid):
+def on_publish(client, userdata, mid):
     """
     What to do when a message is published
     """
     logging.debug("MID " + str(mid) + " published.")
 
 
-def on_subscribe(mosq, obj, mid, qos_list):
+def on_subscribe(client, userdata, mid, granted_qos):
     """
     What to do in the event of subscribing to a topic"
     """
     logging.debug("Subscribe with mid " + str(mid) + " received.")
 
 
-def on_unsubscribe(mosq, obj, mid):
+def on_unsubscribe(client, userdata, mid):
     """
     What to do in the event of unsubscribing from a topic
     """
     logging.debug("Unsubscribe with mid " + str(mid) + " received.")
 
 
-def on_connect(mosq, obj, result_code):
+def on_connect(client, userdata, flags, rc):
     """
     Handle connections (or failures) to the broker.
     This is called after the client has received a CONNACK message
@@ -88,48 +90,48 @@ def on_connect(mosq, obj, result_code):
     4: Refused – bad user name or password (MQTT v3.1 broker only)
     5: Refused – not authorised (MQTT v3.1 broker only)
     """
-    logging.debug("on_connect RC: " + str(result_code))
-    if result_code == 0:
+    logging.debug("on_connect RC: " + str(rc))
+    if rc == 0:
         logging.info("Connected to %s:%s", MQTT_HOST, MQTT_PORT)
         # Publish retained LWT as per
         # http://stackoverflow.com/q/97694
         # See also the will_set function in connect() below
         mqttc.publish(PRESENCETOPIC, "1", retain=True)
         process_connection()
-    elif result_code == 1:
+    elif rc == 1:
         logging.info("Connection refused - unacceptable protocol version")
         cleanup()
-    elif result_code == 2:
+    elif rc == 2:
         logging.info("Connection refused - identifier rejected")
         cleanup()
-    elif result_code == 3:
+    elif rc == 3:
         logging.info("Connection refused - server unavailable")
         logging.info("Retrying in 30 seconds")
         time.sleep(30)
-    elif result_code == 4:
+    elif rc == 4:
         logging.info("Connection refused - bad user name or password")
         cleanup()
-    elif result_code == 5:
+    elif rc == 5:
         logging.info("Connection refused - not authorised")
         cleanup()
     else:
-        logging.warning("Something went wrong. RC:" + str(result_code))
+        logging.warning("Something went wrong. RC:" + str(rc))
         cleanup()
 
 
-def on_disconnect(mosq, obj, result_code):
+def on_disconnect(client, userdata, rc):
     """
     Handle disconnections from the broker
     """
-    if result_code == 0:
+    if rc == 0:
         logging.info("Clean disconnection")
     else:
         logging.info("Unexpected disconnection! Reconnecting in 5 seconds")
-        logging.debug("Result code: %s", result_code)
+        logging.debug("Result code: %s", rc)
         time.sleep(5)
 
 
-def on_message(mosq, obj, msg):
+def on_message(client, userdata, msg):
     """
     What to do when the client recieves a message from the broker
     """
@@ -139,7 +141,7 @@ def on_message(mosq, obj, msg):
     process_message(msg)
 
 
-def on_log(mosq, obj, level, string):
+def on_log(client, userdata, level, string):
     """
     What to do with debug log output from the MQTT library
     """
@@ -171,7 +173,7 @@ def connect():
     logging.debug("Connecting to %s:%s", MQTT_HOST, MQTT_PORT)
     # Set the Last Will and Testament (LWT) *before* connecting
     mqttc.will_set(PRESENCETOPIC, "0", qos=0, retain=True)
-    result = mqttc.connect(MQTT_HOST, MQTT_PORT, 60, True)
+    result = mqttc.connect(MQTT_HOST, MQTT_PORT, 60, "")
     if result != 0:
         logging.info("Connection failed with error code %s. Retrying", result)
         time.sleep(10)
@@ -228,6 +230,8 @@ signal.signal(signal.SIGTERM, cleanup)
 signal.signal(signal.SIGINT, cleanup)
 
 # Connect to the broker
+logging.debug("set user: %s", MQTT_USER)
+mqttc.username_pw_set(MQTT_USER,MQTT_PWD)
 connect()
 
 # Try to loop_forever until interrupted
